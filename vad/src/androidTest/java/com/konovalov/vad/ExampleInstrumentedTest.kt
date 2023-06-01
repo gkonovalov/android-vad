04a94b08b7e391a7ee199117f0c230fbf4bb680e
gkonovalov <22642112+gkonovalov@users.noreply.github.com>
2023-06-01 16:24:47 -0700

    Moved project to Kotlin.

--- vad/src/androidTest/java/com/konovalov/vad/ExampleInstrumentedTest.kt
+++ vad/src/androidTest/java/com/konovalov/vad/ExampleInstrumentedTest.kt
@@ -1,27 +1,22 @@
-package com.konovalov.vad;
+package com.konovalov.vad
 
-import android.content.Context;
-
-import androidx.test.platform.app.InstrumentationRegistry;
-import androidx.test.runner.AndroidJUnit4;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
-import static org.junit.Assert.*;
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
 
 /**
  * Instrumented test, which will execute on an Android device.
  *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ * @see [Testing documentation](http://d.android.com/tools/testing)
  */
-@RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
     @Test
-    public void useAppContext() {
+    fun useAppContext() {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-
-        assertEquals("com.konovalov.vad.test", appContext.getPackageName());
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.konovalov.vad.test", appContext.packageName)
     }
 }
