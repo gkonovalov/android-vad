63bf6a40485d392885d1e8f6d0a62089483f9d65
gkonovalov <22642112+gkonovalov@users.noreply.github.com>
2023-06-01 17:34:58 -0700

    Added comments for FrameSize, Mode, Model, SampleRate, VadSilero

--- vad/src/main/java/com/konovalov/vad/config/Mode.kt
+++ vad/src/main/java/com/konovalov/vad/config/Mode.kt
@@ -1,5 +1,13 @@
 package com.konovalov.vad.config
 
+/**
+ * Created by Georgiy Konovalov on 1/06/2023.
+ * <p>
+ * Enum class representing different detection Modes used in the VAD algorithm. Mode determine
+ * the aggressiveness of the voice activity detection algorithm.
+ * </p>
+ * @property value The numeric value associated with the Mode.
+ */
 enum class Mode(val value: Int) {
     NORMAL(0),
     AGGRESSIVE(2),
